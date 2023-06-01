6b024f0610790907960c78c3d6f8b4c3e5d94d2a
Justin Jones <jus.jones@yahoo.com>
2023-06-01 18:53:26 -0400

    added  useState and useEffect

--- tours_s/src/App.jsx
+++ tours_s/src/App.jsx
@@ -1,6 +1,26 @@
+import { useState, useEffect } from "react";
+
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  useEffect(()=> {
+    setIsLoading(true)
+    const fetchTours = async()=>{
+        try {
+          const response = await fetch(url)
+          const tours = await response.json()
+          console.log(tours);
+      
+        } catch (error) {
+          console.log(error);
+      
+    }
+      }
+      fetchTours();    
+  }, [])
   return <h2>Tours Starter</h2>;
 };
 export default App;
