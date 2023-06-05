39cde91e3669fa48e126aecd8ba3ee9c17154b07
Justin Jones <jus.jones@yahoo.com>
2023-06-05 11:31:54 -0400

    updating the tours and Loading component

--- tours_s/src/App.jsx
+++ tours_s/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Loading from "./Loading";
+import Tours from "./Tours";
 
 const url = 'https://course-api.com/react-tours-project';
 
@@ -33,6 +34,9 @@ const App = () => {
 
 
 
-  return <h2>Tours Starter</h2>;
+  return <main>
+
+    <Tours tours={tours}/>
+  </main>
 };
 export default App;
--- tours_s/src/Loading.jsx
+++ tours_s/src/Loading.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const Loading = () => {
+    
   return (
-    <div>Loading</div>
+    <div className='loading'></div>
   )
 }
 
